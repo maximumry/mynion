fc672c5664147afb380b760f5c77038dae578b3d
maximumry <teapots-thorium.07@icloud.com>
2023-10-13 20:17:07 +0900

    コントローラー名修正

--- app/helpers/mynions_helper.rb
+++
@@ -1,2 +0,0 @@
-module MynionsHelper
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'mynions#index'
-  resources :mynions, only: :index
+  resources :opinions, only: [:index, :new, :create]
 end
