38bee14e269c9713dc74637e9a0f80d6c7832644
maximumry <teapots-thorium.07@icloud.com>
2023-10-14 11:37:51 +0900

    ユーザー管理機能の実装

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,4 +1,5 @@
 class OpinionsController < ApplicationController
+  before_action :authenticate_user, except: :index
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'mynions#index'
-  resources :opinions, only: [:index, :new, :create]
+  resources :opinions, only: :index
 end
