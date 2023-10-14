afe97a87da77e0e6912267f3fac6ec06589526d5
maximumry <teapots-thorium.07@icloud.com>
2023-10-14 14:56:49 +0900

    テストコードの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'mynions#index'
+  root to: 'opinions#index'
   resources :opinions, only: :index
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    :last_name             { Faker::Name.last_name }
+    :first_name            {Faker::Name.first_name}
+    :email                 {Faker::Internet.email}
+    :password              {Faker::Internet.password(min_length: 6)}
+    :password_confirmation {password}
+  end
+end
