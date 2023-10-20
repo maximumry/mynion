40f1c85515227b6b6f513705e8bea2b3294eedef
maximumry <teapots-thorium.07@icloud.com>
2023-10-20 17:21:22 +0900

    showアクションのテスト実施

--- spec/requests/opinions_spec.rb
+++ spec/requests/opinions_spec.rb
@@ -22,4 +22,26 @@ RSpec.describe "Opinions", type: :request do
       expect(response.body).to match(/<img src=".*test_image\.png".*>/)
     end
   end
+  describe 'GET #show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      get opinion_path(@opinion)
+      expect(response.status).to eq 200
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みのツイートのタイトルが存在する' do 
+      get opinion_path(@opinion)
+      expect(response.body).to include(@opinion.title)
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みのテキストが存在する' do 
+      get opinion_path(@opinion)
+      expect(response.body).to include(@opinion.text)
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みの画像が存在する' do 
+      get opinion_path(@opinion)
+      expect(response.body).to match(/<img src=".*test_image\.png".*>/)
+    end
+    it 'showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する' do 
+      get opinion_path(@opinion)
+      expect(response.body).to match('＜コメント一覧＞')
+    end
+  end
 end
