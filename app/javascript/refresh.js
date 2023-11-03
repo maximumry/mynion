c66e0252a18afd1a384362fabdd6ecb413537499
maximumry <teapots-thorium.07@icloud.com>
2023-11-03 17:39:25 +0900

    データベースリセット

--- app/javascript/refresh.js
+++ app/javascript/refresh.js
@@ -1,7 +1,5 @@
 document.addEventListener('turbo:load', () => {
-  console.log("1")
   if (window.location.pathname === '/') {
-    console.log("2")
     setTimeout(() => {
       Turbo.visit(window.location.toString());
     }, 30000);
